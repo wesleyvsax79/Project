b7e78ad57d67f090f33b343f272ac14c8d32dbd5
wesleyvsax79 <wsaxton.prof@gmail.com>
2025-12-08 22:11:08 +0000

    milk and gallon attempt if statement
    My attempt to solve problem using if statement

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("name")
+uk_gallon_to_litres = 4.54609 
+litres_to_batches = 2
+batches_required = 50
+
+litres_to_batches = int(input("How many litres do I require"))
+if litres_to_batches:=50:
+  print("22")
